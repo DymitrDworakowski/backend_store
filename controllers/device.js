31ea5aebfc2365e1a657314e3cf8b5804843620f
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2023-10-26 18:55:20 +0200

    Update device.js

--- controllers/device.js
+++ controllers/device.js
@@ -1,25 +1,31 @@
-const uuid = require('uuid');
+const uuid = require("uuid");
+const path = require("path");
+const { Device } = require("../models/models");
+const ApiError = require("../errors/ApiError");
 
 async function create(req, res, next) {
-    const { name, price, brandId, typeId, info } = req.body;
-    const { img } = req.files;
-    let fileName = uuid.v4() + '.jpg';
-    
-     try {
-    const brand = await Brand.create({ name });
-    return res.status(200).json(brand);
+  const { name, price, brandId, typeId, info } = req.body;
+  const { img } = req.files;
+  let fileName = uuid.v4() + ".jpg";
+  img.mv(path.resolve(__dirname, "..", "static", fileName));
+  try {
+    const device = await Device.create({
+      name,
+      price,
+      brandId,
+      typeId,
+      img: fileName,
+    });
+    return res.json(device);
   } catch (error) {
-    return next(ApiError.internal("Failed to create a Type."));
+    return next(ApiError.internal(error.message));
   }
 }
 
-async function getAll(req, res, next) {
- 
-}
+async function getAll(req, res, next) {}
 
 async function getById(req, res, next) {
-    const {id} = req.query
+  const { id } = req.query;
 }
 
-
-module.exports = {create, getAll,getById};
+module.exports = { create, getAll, getById };
