87dede6eb33e21f41f9e42da8360b77113eb82a8
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2025-09-18 14:05:14 +0200

    Update Product.js

--- shop/models/Product.js
+++ shop/models/Product.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   description: String,
   price: { type: Number, required: true },
   image: String,
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now }, 
+  stock: { type: Number, default: 0 },
+  category: { type: String, required: true },
+  barcode: { type: String, unique: true, sparse: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
